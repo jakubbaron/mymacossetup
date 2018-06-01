06e09ada3bda5cd6fe6853f35921321c13c41f52
Jakub Baron <jakubbaron@jakubs-mbp.mynet>
2018-06-01 20:29:02 +0100

    Commit of the initial configs

---
+++ setup/install.sh
@@ -0,0 +1,11 @@
+### Install homebrew, the missing package manager from macos
+# https://brew.sh/
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+### Install zsh
+# https://medium.com/@mrkdsgn/install-zsh-and-oh-my-zsh-on-osx-10-11-el-capitan-cfaa0ebb97dc 
+brew install zsh zsh-completions
+
+### Install oh-my-zsh
+# http://ohmyz.sh/
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
