7ef7462c4ec6f1c52def6a9d5938daa29c829b18
Jakub Baron <jakubaron@gmail.com>
2019-04-15 16:36:30 +0200

    Adding zsh-autocompletion and zsh-syntax-highlighting to the install setup

--- setup/install.sh
+++ setup/install.sh
@@ -30,3 +30,8 @@ brew cask install font-hack-nerd-font
 brew install boost
 brew install clang-format
 brew install tmux
+
+#Install extra plugins for zsh for oh my zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
